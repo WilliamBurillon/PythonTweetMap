052ac083c513cd1902ac04e77c3d9cf4f06d7879
WilliamBurillon <William.Burillon@etu.univ-savoie.fr>
2019-04-01 18:51:06 +0200

    new version to create a map

--- testfolium.py
+++ testfolium.py
@@ -3,27 +3,43 @@ try:
 except ImportError:
     import simplejson as json
 import mysql.connector
-
 import folium
 
+from TP1 import findCity as fC
+
+
+
+
+
+
+# Connection in the database
 mydb = mysql.connector.connect(host="localhost", user="root", passwd="", database="tweepy")
 mycursor =mydb.cursor()
+
+# get the coordinates in the tweets tables
 mycursor.execute('''SELECT id,`position_X`,`position_Y` FROM `tweets`''')
 
-# for row in mycursor:
-#     print(type(row[1])
-
-
-
-
-
-#
+# use the folium package to make the map and add points relating to the coordinates of the tweets
 fond = r'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'
 carte = folium.Map(location=[46.5, 2.3], zoom_start=6, tiles=fond, attr='© OpenStreetMap © CartoDB')
 
-# fg=folium.FeatureGroup(name="tweet")
 
 for row in mycursor:
-    carte.add_child(folium.Marker(location=[float(row[1]), float(row[2])]))
-# carte.add_child(folium.Marker(location=[6.14,45.9]))
-carte.save(outfile='map.html')
+
+    # loop to put the tweet at the location where it was generate
+    # carte.add_child(folium.Marker(location=[float(row[1]), float(row[2])]))
+
+    #loop to put the tweet in the center of the city where it was generate
+    city_name =fC.getCityName(float(row[1]),float(row[2]))
+    print(city_name)
+    center_coordinate = fC.get_center_coordinate(city_name)
+    if center_coordinate == None:
+
+        carte.add_child(folium.Marker(location=[float(row[1]), float(row[2])]))
+    else:
+
+
+        carte.add_child(folium.Marker(location=center_coordinate))
+
+
+carte.save(outfile='map2.html')
